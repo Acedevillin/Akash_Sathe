4919a2bbbee329a3028a5dda32d3fb3515b653c6
AkashSathe <44794849+Akash-1995@users.noreply.github.com>
2022-04-29 14:46:27 +0100

    updates

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+//NavBar Script
+const tween = gsap.to('.nav-items li', {
+  opacity: 1,
+  duration: 1,
+  x: 0,
+  stagger: 0.7,
+});
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-items');
+  const navLinks = document.querySelectorAll('.nav-items li');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    tween.play(1);
+    burger.classList.toggle('toggle');
+  });
+  //burger animations
+};
+navSlide();
+
+//first section script
 gsap.from('.headline', { opacity: 0, y: 50, duration: 1, stagger: 0.5 });
 
 let t1 = gsap.timeline({
@@ -20,6 +42,75 @@ t1.from('.abt-text', { opacity: 0, y: 50, duration: 1.5, stagger: 0.5 });
 
 const know = document.querySelector('.cta');
 
-know.addEventListener('click', (event) => {
-  location.href('C:PortfolioWebsite(design2)about.html');
+know.addEventListener('click', () => {
+  location.href = './projects.html';
 });
+
+//My work secion animation
+
+//timeline for heading animation
+let t2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.heading',
+    start: 'top center',
+    end: 'bottom bottom',
+    scrub: 1,
+    snap: {
+      snapTo: '.content-2',
+      duration: { min: 0.2, max: 3 },
+      delay: 0.2,
+      ease: 'power1.inoout',
+    },
+  },
+});
+t2.from('.heading', { opacity: 0, y: 50, duration: 1.5 });
+
+//Projects section
+
+let t3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.thumbnail',
+    start: 'top center',
+    end: 'bottom bottom',
+    scrub: 1,
+    snap: {
+      snapTo: '.content-2',
+      duration: { min: 1, max: 3 },
+      delay: 0.5,
+      ease: 'power1.inoout',
+    },
+  },
+});
+t3.from('.thumbnail', { opacity: 0, y: 50, duration: 1.5, stagger: 1.5 })
+  .from('.disheadline', { opacity: 0, y: 50, duration: 1.5, stagger: 1.5 })
+  .from('.discription-text', {
+    opacity: 0,
+    y: 50,
+    duration: 1.5,
+    stagger: 1.5,
+  });
+
+//About Section
+const download = document
+  .querySelector('.cta2')
+  .addEventListener('click', () => {
+    window.open('./Akash_Sathe.pdf', '_blank');
+  });
+
+//Expierence Section Animation
+let t4 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.accordian1',
+    start: 'top center',
+    end: 'bottom bottom',
+    scrub: 1,
+    snap: {
+      snapTo: '.accordian1',
+      duration: { min: 1, max: 3 },
+      delay: 1,
+      ease: 'power1.inoout',
+    },
+  },
+});
+
+t4.from('.accordian1', { opacity: 0, y: 50, duration: 1, stagger: 0.5 });
